5d6bfb6263b80bb96120df7e847101bae9ad75c6
Matlemad <35381854+Matlemad@users.noreply.github.com>
2025-01-28 15:54:13 +0100

    Update TesserAssociativaSBT.sol

--- TesserAssociativaSBT.sol
+++ TesserAssociativaSBT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "./Base64.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MegoTicketsSoulbound is ERC721, Ownable {
     struct Ticket {
